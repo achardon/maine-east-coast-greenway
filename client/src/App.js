d5740f09838917f6a9acb3a986014c1a4aace54d
Alessandra Chardon <achardon@ucla.edu>
2022-02-25 16:09:35 -0500

    React setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    fetch ("/hello")
+    .then(r => r.json())
+    .then(data => setCount(data.count))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Page Count: {count} </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
