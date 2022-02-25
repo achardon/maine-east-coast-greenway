99dc8eb54336e83397df56d6c6655476303bb55c
Alessandra Chardon <achardon@ucla.edu>
2022-02-25 16:03:40 -0500

    react frontend created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
 
     def hello_world
-        session[:count] = (session[:count] || 0) + 1
+        session[:count] ||= 0
+        session[:count] = session[:count] + 1
         render json: { count: session[:count] }
       end
 end
