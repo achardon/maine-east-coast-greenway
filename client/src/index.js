d6416410f7964932a4551b28ea1e5181ddc70199
Alessandra Chardon <achardon@ucla.edu>
2022-03-02 16:35:50 -0500

    add Navbar component

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './components/App';
 import About from './components/About';
+import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -10,8 +11,9 @@ ReactDOM.render(
   <React.StrictMode>
     
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        
+
         <Route path="/" element={<App />}></Route>
         <Route path="/about" element={<About />}></Route>
         
