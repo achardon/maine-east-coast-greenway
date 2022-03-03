e24e3dd97687476c4c32fdbfc612579e7cae6c24
Alessandra Chardon <achardon@ucla.edu>
2022-03-03 09:02:19 -0500

    fix react-bootstrap installation and add explore route

---
+++ client/src/components/NavHeader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+function NavHeader() {
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">East Coast Greenway</Navbar.Brand>
+        <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="about">About</Nav.Link>
+        <Nav.Link href="explore">Explore</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavHeader
--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './components/App';
+import NavHeader from './components/NavHeader';
 import About from './components/About';
-import Navbar from './components/Navbar';
+import Explore from './components/Explore';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     
     <BrowserRouter>
-      <Navbar />
+      <NavHeader />
       <Routes>
 
         <Route path="/" element={<App />}></Route>
         <Route path="/about" element={<About />}></Route>
-        
+        <Route path="/explore" element={<Explore />}></Route>
+
       </Routes>
     </BrowserRouter>
 
