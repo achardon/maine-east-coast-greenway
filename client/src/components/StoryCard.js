1cb6785eb13ac91e5d758b115bbcd321ddd1653b
Alessandra Chardon <achardon@ucla.edu>
2022-11-08 21:03:07 -0500

    add story card component and color change capability

---
+++ client/src/components/StoryCard.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+
+function StoryCard( {story} ) {
+
+    const [selectedColor, setSelectedColor] = useState("white");
+
+    function handleClick(e, story) {
+        if (selectedColor === "white") {
+            setSelectedColor("orange");
+        } else {
+            setSelectedColor("white");
+        }
+    }
+
+    return (
+      <div>
+        <Card
+          key={story.id}
+          style={{ width: "30rem", backgroundColor: selectedColor }}
+          className="m-3"
+          onClick={(e) => handleClick(e, story)}
+        >
+          <Card.Body>
+            <Card.Title>{story.date}</Card.Title>
+            <Card.Img
+              variant="top"
+              src={
+                "https://static.owayo-cdn.com/newhp/img/magazin/planningbiketrip/expert-tips-for-planning-your-bike-trip-670px.jpg"
+              }
+              height="280px"
+            />
+            <Card.Text>{story.narrative}</Card.Text>
+            <Card.Text>
+              <strong>-{story.name}</strong>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+}
+
+export default StoryCard
